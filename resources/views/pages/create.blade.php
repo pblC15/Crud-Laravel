334f5178f11b2ef959d1f522426caa600c3949cf
Pablo Cassiano <pabloboyreis@hotmail.com>
2021-02-09 22:59:55 -0300

    Implementando login

--- resources/views/pages/create.blade.php
+++ resources/views/pages/create.blade.php
@@ -6,47 +6,45 @@
 
 <div class="col-5 m-auto">
 
-<form action="{{ route('create.do')}}" method='post' name="formCad" id="formCad"> 
-@csrf 
+  <form action="{{ route('create.do')}}" method='post' name="formCad" id="formCad"> 
+  @csrf 
 
-  <div class="form-group">
-    <label for="id_title">Título</label>
-    <input type="text" class="form-control" name="title" id="id_title" placeholder="ex: Os miseráveis">
-  </div>
-
-  <div class="form-group">
-    <label for="id_user">Escolha o Autor</label>
-    <select class="form-control" id="id_user" name="id_user">
-      <option value=''></option>
-      @foreach($users as $user)
-
-      <option value='{{ $user->id}}'>{{ $user->name }}</option>
-      
-      @endforeach
-
-    </select>
-  </div>
-
-  <div class="form-group">
-    <label for="id_pages">Páginas</label>
-    <input type="text" class="form-control" name="pages" id="id_pages" placeholder="ex: 500">
-  </div>
-  
-  <div class="form-group">
-    <label for="id_price">Preço</label>
-    <input type="text" class="form-control" name="price" id="id_price" placeholder="ex: 155.99">
-  </div>
-  <button type="submit" class="btn btn-success">Cadastrar</button>
-
-</form>
-    <div class="text-right">
-    
-        <a href="{{ route('home')}}">
-            <button type='submit' class="btn btn-primary">Voltar</button>
-        </a>
+    <div class="form-group">
+      <label for="id_title">Título</label>
+      <input type="text" class="form-control" name="title" id="id_title" placeholder="ex: Os miseráveis" required="required">
     </div>
+
+    <div class="form-group">
+      <label for="id_user">Escolha o Autor</label>
+      <select class="form-control" id="id_user" name="id_user" required="required">
+        <option value=''></option>
+        @foreach($users as $user)
+
+        <option value='{{ $user->id}}'>{{ $user->name }}</option>
+        
+        @endforeach
+
+      </select>
+    </div>
+
+    <div class="form-group">
+      <label for="id_pages">Páginas</label>
+      <input type="text" class="form-control" name="pages" id="id_pages" placeholder="ex: 500" required="required">
+    </div>
+    
+    <div class="form-group">
+      <label for="id_price">Preço</label>
+      <input type="text" class="form-control" name="price" id="id_price" placeholder="ex: 155.99" required="required">
+    </div>
+
+    <div class="text-left">
+    <button type="submit" class="btn btn-success">Cadastrar</button>
+      <a class="btn btn-primary" href="{{ route('home')}}">
+          Voltar
+      </a>
+    </div>
+
+  </form>
 </div>
-
-
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\BookController@index')->name('home');
+Route::get('/home', 'App\Http\Controllers\BookController@index')->name('home');
 Route::get('/books/{id}', 'App\Http\Controllers\BookController@show')->name('view');
 Route::get('/create', 'App\Http\Controllers\BookController@create')->name('create');
 Route::post('/create.do', 'App\Http\Controllers\BookController@store')->name('create.do');
 Route::get('/edit/{id}', 'App\Http\Controllers\BookController@edit')->name('edit');
 Route::post('/edit.do/{id}', 'App\Http\Controllers\BookController@update')->name('edit.do');
 Route::get('/delete{id}', 'App\Http\Controllers\BookController@destroy')->name('delete');
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('login');
